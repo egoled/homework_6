3b93e7cb7f91ba7ff2642037ed8745b36c0db435
«Egor <egoled8@gmail.com>
2022-06-30 01:37:35 +0300

    первая задача от 23 июня

---
+++ Exercise_1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int[] InputNumbers(int countM)
+{
+    int[] MNumber = new int[countM];
+    for (int i = 0; i < countM; i++)
+    {
+        MNumber[i] = prompt($"Введите {i + 1} из {countM} чисел ");
+    }
+    return MNumber;
+}
+int GetPositiveElements(int[] array)
+{
+    int count = 0;
+    foreach (int el in array)
+    {
+        if (el > 0) count++;
+    }
+    return count;
+}
+
+
+int M = prompt("Введите количество чисел, из которых состоит массив  ");
+int[] newarray = InputNumbers(M);
+Console.WriteLine($"Вы ввели {GetPositiveElements(newarray)} положительных чисел");
